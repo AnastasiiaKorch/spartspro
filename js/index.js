ea448dc9b179473728d1819928e1c4b2c49768e5
AnastasiiaKorch <anastasia_92_92@list.ru>
2025-10-16 20:44:12 +0300

    add tablet+mobile

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 const burger = document.querySelector('.header__burger');
 const nav = document.querySelector('.header__nav');
+const body = document.body;
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('is-open');
-    burger.classList.toggle('is-active'); // Добавляем класс is-active бургеру
+    burger.classList.toggle('is-active');
+    body.classList.toggle('no-scroll');
 });
+
